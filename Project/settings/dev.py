7e5e254fa428ed18ac43bda252e109069508412b
Pratik Mukherjee <codingtricks.csc@gmail.com>
2023-09-20 00:16:06 +0530

    Test

--- Project/settings/dev.py
+++ Project/settings/dev.py
@@ -6,13 +6,9 @@ DEBUG = True
 SECRET_KEY = "django-insecure-bue!n4j58tr&12k^b(!rl089ju^i#x-fc(d841_cux+=bjw_g!"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "basic_bank",
-        "HOST": "localhost",
-        "PORT": "3306",
-        "USER": "root",
-        "PASSWORD": "2001"
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
